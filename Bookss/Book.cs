5b4fe44f9740fe216bb52ca95760c80be8811b17
Анастасия <anastasiya.martynova@phystech.edu>
2017-12-31 02:33:24 +0300

    Books

---
+++ Bookss/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookss
+{
+    class Book
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual IList<Author> Authors { get; set; }
+
+        public virtual double Rating { get; set; }
+
+        public Book()
+        {
+
+        }
+
+
+
+
+    }
+    class Author
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual IList<Book> Books { get; set; }
+
+    }
+}
